3cd3fbef6f2ea0316da32289799ec9e43edb3a16
Richard <rpmagdaluyo@gmail.com>
2018-01-23 22:23:10 -0800

    Initial commit

---
+++ Lab 2 - Vending Machine.srcs/sources_1/imports/New folder/lab2b.vhd
@@ -0,0 +1,30 @@
+﻿----------------------------------------------------------------------
+-- EECS31L/CSE31L Assignment2
+-- FSM Behavioral Model
+----------------------------------------------------------------------
+-- Student First Name : Your First Name
+-- Student Last Name : Your Last Name
+-- Student ID : Your Student ID
+----------------------------------------------------------------------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+entity Lab2b_FSM is
+    Port ( Input : in  STD_LOGIC_VECTOR(2 DOWNTO 0);
+           Clk : in  STD_LOGIC;
+           Permit : out  STD_LOGIC;
+           ReturnChange : out  STD_LOGIC);
+end Lab2b_FSM;
+
+architecture Behavioral of Lab2b_FSM is
+
+-- DO NOT modify any signals, ports, or entities above this line
+-- Create 2 processes (one for updating state status and the other for describing transitions and outputs)
+-- Figure out the appropriate sensitivity list of both the processes.
+-- Use CASE statements and IF/ELSE/ELSIF statements to describe your processes.
+-- add your code here
+
+END Behavioral;
---
+++ Lab 2 - Vending Machine.srcs/sources_1/imports/New folder/lab2b_tb.vhd
@@ -0,0 +1,120 @@
+--------------------------------------------------------------------------------
+-- Company:       UCI EECS 31L
+-- Engineer:      QV
+--
+-- Create Date:   14:25:53 1/22/2018
+-- Design Name:   
+-- Module Name:   D:/Xilinx/f13/lab2_permit/lab2b_tb.vhd
+-- Project Name:  lab2_permit
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Lab2b_FSM
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY lab2b_tb IS
+END lab2b_tb;
+ 
+ARCHITECTURE behavior OF lab2b_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Lab2b_FSM
+    PORT(
+         Input : IN  std_logic_vector(2 downto 0);
+         Clk : IN  std_logic;
+         Permit : OUT  std_logic;
+         ReturnChange : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal Input : std_logic_vector(2 downto 0) := (others => '0');
+   signal Clk : std_logic := '0';
+
+ 	--Outputs
+   signal Permit : std_logic;
+   signal ReturnChange : std_logic;
+
+   -- Clock period definitions
+   constant Clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Lab2b_FSM PORT MAP (
+          Input => Input,
+          Clk => Clk,
+          Permit => Permit,
+          ReturnChange => ReturnChange
+        );
+
+   -- Clock process definitions
+   Clk_process: process
+   begin
+		Clk <= '0';
+		wait for Clk_period/2;
+		Clk <= '1';
+		wait for Clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      -- insert stimulus here 
+
+        -- ignoring any setup/hold time issues for behavior --
+		-- TEST CASE 0 BEGIN (provided) --
+		
+		-- insert $10 ($10 total)
+		Input <= "010";
+		Wait for 20 NS ;
+		
+	   Input <= "000";
+		Wait for 20 NS ;
+		
+		-- insert $5 ($15 total)
+	   Input <= "001";
+		Wait for 20 NS ;
+		
+	   Input <= "000";
+		Wait for 20 NS ;
+		
+		-- insert $5 ($20 total, expecting P=1, R=0)
+	   Input <= "001";
+		Wait for 10 NS ;
+		ASSERT Permit = '1' REPORT "p = 1 fail with test case 0" SEVERITY WARNING;
+		ASSERT ReturnChange = '0' REPORT "r = 0 fail with test case 0" SEVERITY WARNING;
+		Wait for 10 NS ;
+		
+        -- more test cases here
+		
+      wait;
+   end process;
+
+END;
