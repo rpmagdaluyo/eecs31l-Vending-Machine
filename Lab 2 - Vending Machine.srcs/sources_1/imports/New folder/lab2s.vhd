38614dff19d04ea30761db1f40bca031f8bbd497
Richard <rpmagdaluyo@yahoo.com>
2018-02-06 09:46:13 -0800

    Laptop Work
    Started structural code

--- Lab 2 - Vending Machine.srcs/sources_1/imports/New folder/lab2s.vhd
+++ Lab 2 - Vending Machine.srcs/sources_1/imports/New folder/lab2s.vhd
@@ -1,10 +1,10 @@
-﻿----------------------------------------------------------------------
+----------------------------------------------------------------------
 -- EECS31L/CSE31L Assignment2
 -- FSM Structural Model
 ----------------------------------------------------------------------
--- Student First Name : Your First Name
--- Student Last Name : Your Last Name
--- Student ID : Your Student ID
+-- Student First Name : Richard
+-- Student Last Name : Magdaluyo
+-- Student ID : 45993793
 ----------------------------------------------------------------------
 
 LIBRARY ieee;
@@ -24,6 +24,27 @@ ARCHITECTURE Structural OF Lab2s_FSM IS
 -- For the combinatorial process, use Boolean equations consisting of AND, OR, and NOT gates while expressing the delay in terms of the NAND gates. 
 -- For the state register process, use IF statements.
 -- Figure out the appropriate sensitivity list of both the processes.
--- add your code here
-
+-- add your codehere
+    SUBTYPE Statetype IS std_logic_vector(1 DOWNTO 0);
+    CONSTANT S_Off : Statetype := "00";
+    SIGNAL CurrState, NextState: Statetype;
+    
+    -- CombLogic updates status
+    CombLogic: process(CurrState, Input)
+    begin
+        Permit <= ((CurrState(2) and not(CurrState(1)) and not(CurrState(0)));  
+        ReturnChange <= ((CurrState(3) and CurrState(2) and not(CurrState(1))));
+        NextState(3) <= 
+        NextState(2) <= 
+        NextState(1) <= 
+        NextState(0) <=         
+    end process CombLogic;
+    
+    -- StateReg is the clock
+    StateReg: process(Clk)
+    begin
+        if(Clk = '1' and Clk'event) then
+            CurrState <= NextState after 5.6ns;
+        end if;
+    end process StateReg;    
 END Structural;
--- Lab 2 - Vending Machine.srcs/sources_1/imports/New folder/lab2s_tb.vhd
+++ Lab 2 - Vending Machine.srcs/sources_1/imports/New folder/lab2s_tb.vhd
@@ -2,15 +2,15 @@
 -- Company:       UCI EECS 31L
 -- Engineer:      QV
 --
--- Create Date:   15:03:20 1/22/2018
+-- Create Date:   14:25:53 1/22/2018
 -- Design Name:   
--- Module Name:   D:/Xilinx/f13/lab2_permit/lab2s_tb.vhd
+-- Module Name:   D:/Xilinx/f13/lab2_permit/lab2b_tb.vhd
 -- Project Name:  lab2_permit
 -- Target Device:  
 -- Tool versions:  
 -- Description:   
 -- 
--- VHDL Test Bench Created by ISE for module: Lab2s_FSM
+-- VHDL Test Bench Created by ISE for module: Lab2b_FSM
 -- 
 -- Dependencies:
 -- 
@@ -32,14 +32,14 @@ USE ieee.std_logic_1164.ALL;
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
  
-ENTITY lab2s_tb IS
-END lab2s_tb;
+ENTITY lab2b_tb IS
+END lab2b_tb;
  
-ARCHITECTURE behavior OF lab2s_tb IS 
+ARCHITECTURE behavior OF lab2b_tb IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
-    COMPONENT Lab2s_FSM
+    COMPONENT Lab2b_FSM
     PORT(
          Input : IN  std_logic_vector(2 downto 0);
          Clk : IN  std_logic;
@@ -57,14 +57,13 @@ ARCHITECTURE behavior OF lab2s_tb IS
    signal Permit : std_logic;
    signal ReturnChange : std_logic;
 
-   -- Clock period definitions 
-   -- MAKE SURE TO UPDATE FOR STRUCTURAL
+   -- Clock period definitions
    constant Clk_period : time := 10 ns;
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
-   uut: Lab2s_FSM PORT MAP (
+   uut: Lab2b_FSM PORT MAP (
           Input => Input,
           Clk => Clk,
           Permit => Permit,
@@ -72,7 +71,7 @@ BEGIN
         );
 
    -- Clock process definitions
-   Clk_process :process
+   Clk_process: process
    begin
 		Clk <= '0';
 		wait for Clk_period/2;
@@ -86,18 +85,78 @@ BEGIN
    begin		
       -- hold reset state for 100 ns.
       wait for 100 ns;	
-      
-      
-        -- waiting for next clock edge, then adding a little bit of time
-        -- avoiding any potential setup/hold time errors
-        -- you can remove these next 2 lines and/or modify them as you wish
-        WAIT UNTIL clk = '1' AND clk'EVENT;
-		wait for 2 ns;
 
       -- insert stimulus here 
+
+        -- ignoring any setup/hold time issues for behavior --
+		-- TEST CASE 0 BEGIN (provided) --
 		
-		-- TEST CASE 0 BEGIN --
+		-- insert Ten ($10 Total)
+		Input <= "010";
+		Wait for 20 NS ;
 		
+	    Input <= "000";
+		Wait for 20 NS ;
+		
+		-- insert Five ($15 Total)
+	    Input <= "001";
+		Wait for 20 NS ;
+		
+	    Input <= "000";
+		Wait for 20 NS ;
+		
+		-- insert Five ($20 Total, expecting P=1, R=0)
+	    Input <= "001";
+		Wait for 10 NS ;
+		ASSERT Permit = '1' REPORT "p = 1 fail with test case 0" SEVERITY WARNING;
+		ASSERT ReturnChange = '0' REPORT "r = 0 fail with test case 0" SEVERITY WARNING;
+		Wait for 10 NS ;
+		
+		Input <= "000";
+		wait for 10ns;
+		
+        -- more test cases here
+        
+		-- insert Twenty ($20 Total, expecting P=1, R=0) 
+		Input <= "100";
+		wait for 10ns;
+		assert Permit = '1' report "p = 1 fail with test case 1" severity warning;
+        assert ReturnChange = '0' report "r = 0 fail with test case 1" severity warning;		
+        wait for 10ns;
+        
+        Input <= "000";
+        wait for 20ns;
+
+		-- insert NoBill after getting the permit;
+		Input <= "000";
+		wait for 10ns;
+		assert Permit = '0' report "p = 0 fail with test case 2" severity warning;
+        assert ReturnChange = '0' report "r = 0 fail with test case 2" severity warning;
+        wait for 10ns;
+		
+		-- insert Five ($5 Total;
+		Input <= "001";
+		wait for 20ns;
+        
+        Input <= "000";
+        wait for 20ns;
+		
+		-- insert Ten;
+		Input <= "010";
+		wait for 20ns;
+		
+		Input <= "000";
+		wait for 20ns;
+		
+		-- insert Twenty ($25 Total, expecting P=1, R=1)
+		Input <= "100";
+		wait for 20ns;
+		
+		Input <= "000";
+		wait for 10ns;
+		assert Permit = '1' report "p = 1 fail with test case 3" severity warning;
+        assert ReturnChange = '1' report "r = 1 fail with test case 3" severity warning;
+        wait for 10ns;
       wait;
    end process;
 
